2fcfa37dad03d2f9d861220c9afee9e69b9eec71
Omer Turhan <devturhan@gmail.com>
2025-08-10 22:27:34 +0300

    Initial commit

---
+++ KYCFlowApp/KYCFlowAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KYCFlowAppApp.swift
+//  KYCFlowApp
+//
+//  Created by Ömer Turhan on 10.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct KYCFlowAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
